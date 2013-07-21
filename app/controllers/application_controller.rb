519b7d4107dfb24aa22381b00573382f58825de6
David McCaffrey <root@redxent.org>
2013-07-20 22:19:09 -0500

    Integrated functionality from previous version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	before_filter :init_function
+
+	def init_function
+		@controller = controller_name
+		@action = action_name
+	end
+  
 end
